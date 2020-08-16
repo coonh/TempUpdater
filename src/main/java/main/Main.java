8c407cec1b3d6d50972b9aef5a2a759278fd12a6
Coonh <coonh@kabelmail.de>
2020-08-17 00:32:38 +0200

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Updater u1 = new Updater();
+        u1.start();
+
+    }
+}
---
+++ src/main/java/main/Updater.java
@@ -0,0 +1,92 @@
+package main;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.concurrent.*;
+
+public class Updater extends Thread{
+
+    private String tempFileLocation = "data";
+    private String JSONLocation = "data/data.json";
+    private String path = "data/ips.txt";
+    private ArrayList<String> ips;
+    private ArrayList<String> paths;
+    private Boolean running = false;
+
+    public Updater(){
+        File l = new File(path);
+        ips = new ArrayList<>();
+        paths = new ArrayList<>();
+        String content="";
+        try {
+           BufferedReader r = new BufferedReader(new FileReader(l));
+           String st;
+           while ((st =r.readLine())!=null) {
+               ips.add(st);
+           }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String st:ips) {
+            paths.add(tempFileLocation+"/"+st+"/w1_slave");
+        }
+
+
+    }
+
+    @Override
+    public synchronized void start() {
+        ScheduledExecutorService s = Executors.newScheduledThreadPool(1);
+        Runnable task2 = () -> update();
+        s.scheduleAtFixedRate(task2,0,2500,TimeUnit.MILLISECONDS);
+
+    }
+
+    private void update() {
+        JSONObject obj = new JSONObject();
+        for (int i = 0; i < ips.size(); i++) {
+            try {
+                BufferedReader r = new BufferedReader(new FileReader(new File(paths.get(i))));
+                r.readLine();
+                String line = r.readLine();
+                int t = line.indexOf('t');
+                float number = Integer.parseInt(line.substring(t + 2, line.length()));
+                number = Math.round(number/1000);
+                r.close();
+
+                if (i < 11) {
+                    obj.put("w1_" + (i+1), number);
+                } else {
+                    obj.put("w2_"+(i+1-ips.size()/2), number);
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(obj.toString());
+        writeJSONFile(obj);
+    }
+
+    private void writeJSONFile(JSONObject obj) {
+        try {
+            FileWriter fw = new FileWriter(JSONLocation);
+            fw.write(obj.toString());
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
