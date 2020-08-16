0547d32873f65b6475907926c0e581982da23bf1
Coonh <coonh@kabelmail.de>
2020-08-17 01:52:15 +0200

    stop in console and path in external file

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,36 @@
 package main;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Updater u1 = new Updater();
         u1.start();
+        Thread t1 = new Thread(){
+            @Override
+            public synchronized void start() {
+                while (u1.running){
+                    try{
+                        String input = scanner.nextLine();
+                        if(!input.isEmpty()){
+                            System.out.println("Got input: "+input);
+                            if(input.equals("stop")||input.equals("quit")||input.equals("q")){
+                                System.out.println("Shutting down...");
+                                u1.exit();
+                                System.out.println("System shutdown with no Errors!");
+                            }
+                        }
 
+                    }catch (NoSuchElementException e){
+
+                    }
+                }
+            }
+        };
+        t1.start();
     }
 }
--- src/main/java/main/Updater.java
+++ src/main/java/main/Updater.java
@@ -9,12 +9,13 @@ import java.util.concurrent.*;
 
 public class Updater extends Thread{
 
-    private String tempFileLocation = "data";
+    private String tempFileLocation;
     private String JSONLocation = "data/data.json";
     private String path = "data/ips.txt";
     private ArrayList<String> ips;
     private ArrayList<String> paths;
-    private Boolean running = false;
+    public Boolean running = false;
+    private ScheduledExecutorService scheduler;
 
     public Updater(){
         File l = new File(path);
@@ -27,6 +28,9 @@ public class Updater extends Thread{
            while ((st =r.readLine())!=null) {
                ips.add(st);
            }
+           r = new BufferedReader(new FileReader(new File("data/path.txt")));
+           tempFileLocation = r.readLine();
+            System.out.println(tempFileLocation);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -43,40 +47,50 @@ public class Updater extends Thread{
 
     @Override
     public synchronized void start() {
-        ScheduledExecutorService s = Executors.newScheduledThreadPool(1);
-        Runnable task2 = () -> update();
-        s.scheduleAtFixedRate(task2,0,2500,TimeUnit.MILLISECONDS);
+        scheduler = Executors.newScheduledThreadPool(1);
+        running = true;
+        Runnable task2 = () -> {
+            update();
+        };
+        scheduler.scheduleAtFixedRate(task2,0,2500,TimeUnit.MILLISECONDS);
 
     }
 
     private void update() {
-        JSONObject obj = new JSONObject();
-        for (int i = 0; i < ips.size(); i++) {
-            try {
-                BufferedReader r = new BufferedReader(new FileReader(new File(paths.get(i))));
-                r.readLine();
-                String line = r.readLine();
-                int t = line.indexOf('t');
-                float number = Integer.parseInt(line.substring(t + 2, line.length()));
-                number = Math.round(number/1000);
-                r.close();
+            JSONObject obj = new JSONObject();
+            for (int i = 0; i < ips.size(); i++) {
+                try {
+                    BufferedReader r = new BufferedReader(new FileReader(new File(paths.get(i))));
+                    r.readLine();
+                    String line = r.readLine();
+                    int t = line.indexOf('t');
+                    float number = Integer.parseInt(line.substring(t + 2, line.length()));
+                    number = Math.round(number/1000);
+                    r.close();
 
-                if (i < 11) {
-                    obj.put("w1_" + (i+1), number);
-                } else {
-                    obj.put("w2_"+(i+1-ips.size()/2), number);
+                    if (i < 11) {
+                        obj.put("w1_" + (i+1), number);
+                    } else {
+                        obj.put("w2_"+(i+1-ips.size()/2), number);
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
-        }
-        System.out.println(obj.toString());
-        writeJSONFile(obj);
+            System.out.println(obj.toString());
+            writeJSONFile(obj);
+
+    }
+
+    public void exit(){
+        running=false;
+        scheduler.shutdown();
+        System.out.println("Scheduled task has shut down.");
     }
 
     private void writeJSONFile(JSONObject obj) {
